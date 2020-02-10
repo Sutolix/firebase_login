121231733d520cdb3ae29caefa229e58d7c5e73e
Pocchi <56132780+Sutolix@users.noreply.github.com>
2020-02-10 09:45:24 -0300

    Security update

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,16 +2,12 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+/*
 const firebaseConfig = {
-  apiKey: "AIzaSyDMRkIDyXdqvLy6rCw_38x98vkB50kZVUk",
-  authDomain: "control-f-8eb4f.firebaseapp.com",
-  databaseURL: "https://control-f-8eb4f.firebaseio.com",
-  projectId: "control-f-8eb4f",
-  storageBucket: "control-f-8eb4f.appspot.com",
-  messagingSenderId: "910065145671",
-  appId: "1:910065145671:web:01eef19e5655229b4d1e62",
-  measurementId: "G-WHFHZEDYJF"
+  Firebase database info
 };
+
+*/
 
 class Firebase {
 
